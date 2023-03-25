a338bd79d8dabb4cad62018444e60824255fa4ac
Gabriela K <97364425+GabrielaLokelani@users.noreply.github.com>
2023-03-24 20:15:43 -1000

    Add Solidity contract

---
+++ CatRescue.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: Gabriela Kadzielawa 2023
+// compiler version must be greater than or equal to 0.8.3 and less than 0.9.0
+pragma solidity ^0.8.3;
+
+contract CatRescue {
+
+    address owner = msg.sender;
+    uint balance = address(this).balance;
+    mapping(address => uint) adopted;
+    uint returnExpiration;
+
+    modifier isAllowed(uint catIndex) {
+        require(cat[catIndex].available == true, "Sorry! This cat has been adopted already");
+        _;
+    } 
+
+    struct Cat {
+        string catsName;
+        string gender;
+        uint age;
+        string description;
+        string hash;
+        bool available;   
+    }
+    Cat[] public cat;
+
+    struct Customer {
+        string gender;
+        uint age;
+        string name;
+        string pet;
+        uint adoptionTime;
+    }
+    mapping (address => Customer) private Customers;
+
+    constructor() public {
+        adopted[msg.sender] = 0;
+    }
+
+    function add(string memory _catName, string memory _catGender, uint _catAge, string memory _description, string memory _hash, bool _available) public {
+        require(msg.sender == owner, "Sorry! Only the owner may shelter cats.");
+
+        cat.push(Cat({
+            catsName : _catName,
+            gender : _catGender,
+            age : _catAge,
+            description : _description,
+            hash : _hash,
+            available : _available
+        }));
+    }
+
+    function adopt(string memory personName, string memory personGender, uint personAge, string memory _catsName, uint catIndex) public isAllowed(catIndex) returns(bool adoptionSuccess) {
+        require(adopted[msg.sender] == 0, "You already adopted!");
+        require(keccak256(abi.encodePacked(_catsName)) == keccak256(abi.encodePacked(cat[catIndex].catsName)), "Sorry! Cat's name does not match with the index given");
+
+        Customers[msg.sender].gender = personGender;
+        Customers[msg.sender].age = personAge;
+        Customers[msg.sender].name = personName;
+        Customers[msg.sender].pet = _catsName;
+        cat[catIndex].available = false;
+        Customers[msg.sender].adoptionTime = block.timestamp;
+        adopted[msg.sender]++;
+        return true;
+    }
+
+    function returnAnimalToShelter(uint catIndex) public returns(bool returnSuccess) {
+        returnExpiration = Customers[msg.sender].adoptionTime + 1 days;
+        require(block.timestamp <= returnExpiration, "Returns must be within 24 hours of adoption!");
+
+        Customers[msg.sender].pet = "RETURNED";
+        cat[catIndex].available = true;
+        adopted[msg.sender]--;
+        return true;
+    }
+
+    function donate(uint256 value) public payable {
+        balance += value;
+    }
+
+    function getCatsCount() public view returns (uint256 catCount) {
+        return cat.length;
+    }
+
+    function getCat(uint256 catIndex) public view returns(string memory name, string memory gender, uint age, string memory description, string memory _hash, bool available) {
+        Cat memory specificCat = cat[catIndex];
+        return(specificCat.catsName, specificCat.gender, specificCat.age, specificCat.description, specificCat.hash, specificCat.available);
+    }
+
+    function getReceipt() public view returns(string memory customers_name, uint customers_age, string memory customers_gender, string memory customers_pet) {
+        return(Customers[msg.sender].name, Customers[msg.sender].age, Customers[msg.sender].gender, Customers[msg.sender].pet);
+    }
+
+    function getDonationBalance() public view returns(uint donationTotal) {
+        return balance;
+    }
+}
